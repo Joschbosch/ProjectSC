49c5b2151e7be08455443427c67835e9da550d56
Josch <josch@joschbosch.de>
2015-06-30 16:24:57 +0200

    Added new houeses & editor correct

---
+++ src/de/projectsc/Editor.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (C) 2015
+ */
+
+package de.projectsc;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import de.projectsc.editor.Editor3DCore;
+
+/**
+ * World editor.
+ * 
+ * @author Josch Bosch
+ */
+public class Editor extends JFrame {
+
+    /**
+     * UID.
+     */
+    private static final long serialVersionUID = 1L;
+
+    private JFrame frame;
+
+    private final JPanel panel1 = new JPanel();
+
+    private final JPanel panel2 = new JPanel();
+
+    private Canvas displayParent;
+
+    private Thread gameThread;
+
+    private Editor3DCore editor3dCore;
+
+    BlockingQueue<String> messageQueue;
+
+    public Editor() {}
+
+    /**
+     * Once the Canvas is created its add notify method will call this method to start the LWJGL
+     * Display and game loop in another thread.
+     */
+    public void startLWJGL() {
+        messageQueue = new LinkedBlockingQueue<String>();
+        editor3dCore = new Editor3DCore(displayParent, WIDTH, HEIGHT, messageQueue);
+        gameThread = new Thread(editor3dCore);
+        gameThread.start();
+    }
+
+    /**
+     * Tell game loop to stop running, after which the LWJGL Display will be destoryed. The main
+     * thread will wait for the Display.destroy() to complete
+     */
+    private void stopLWJGL() {
+        try {
+            editor3dCore.stop();
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private void execute() {
+        frame.setVisible(true);
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                frame.remove(displayParent);
+                frame.dispose();
+            }
+        });
+        frame.setBounds(100, 100, 1024, 768);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new GridLayout(0, 2, 0, 0));
+        frame.getContentPane().add(panel1);
+        panel1.setLayout(null);
+
+        JButton button = new JButton("Load Map");
+        button.setBounds(0, 0, 100, 50);
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                messageQueue.offer("Load Map");
+
+            }
+        });
+        panel1.add(button);
+
+        frame.getContentPane().add(panel2);
+        panel2.setLayout(new BorderLayout(0, 0));
+
+        displayParent = new Canvas() {
+
+            /**
+             * UID.
+             */
+            private static final long serialVersionUID = -6934397843291223284L;
+
+            @Override
+            public void addNotify() {
+                super.addNotify();
+                startLWJGL();
+            }
+
+            @Override
+            public void removeNotify() {
+                stopLWJGL();
+                super.removeNotify();
+            }
+        };
+        displayParent.setFocusable(true);
+        displayParent.requestFocus();
+        displayParent.setIgnoreRepaint(true);
+        panel2.add(displayParent);
+    }
+
+    /**
+     * Main.
+     * 
+     * @param args program args
+     * */
+    public static void main(String[] args) {
+        Editor sit = new Editor();
+        sit.initialize();
+        sit.execute();
+    }
+}
---
+++ src/de/projectsc/editor/Editor3DCore.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2015 Project SC
+ * 
+ * All rights reserved
+ */
+package de.projectsc.editor;
+
+import java.awt.Canvas;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector4f;
+
+import de.projectsc.client.gui.objects.Camera;
+import de.projectsc.client.gui.objects.GraphicalEntity;
+import de.projectsc.client.gui.objects.Light;
+import de.projectsc.client.gui.render.MasterRenderer;
+import de.projectsc.client.gui.terrain.TerrainModel;
+import de.projectsc.client.gui.textures.TerrainTexture;
+import de.projectsc.client.gui.textures.TerrainTexturePack;
+import de.projectsc.client.gui.tools.Loader;
+import de.projectsc.client.gui.tools.MousePicker;
+import de.projectsc.core.Terrain;
+import de.projectsc.core.TerrainLoader;
+
+/**
+ * Core class for the GUI.
+ * 
+ * @author Josch Bosch
+ */
+public class Editor3DCore implements Runnable {
+
+    private boolean running;
+
+    private final Canvas displayParent;
+
+    private final int width;
+
+    private final int height;
+
+    private Loader loader;
+
+    private MasterRenderer masterRenderer;
+
+    private Camera camera;
+
+    private MousePicker mousePicker;
+
+    private TerrainModel terrainModel;
+
+    private List<Light> lights;
+
+    private Object staticEntities;
+
+    private final BlockingQueue<String> messageQueue;
+
+    public Editor3DCore(Canvas displayParent, int width, int height, BlockingQueue<String> messageQueue) {
+
+        this.displayParent = displayParent;
+        this.width = width;
+        this.height = height;
+        this.messageQueue = messageQueue;
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        try {
+            Display.setDisplayMode(new DisplayMode(width, height));
+            Display.setTitle("Project SC Editor");
+            Display.setVSyncEnabled(true);
+            Display.setParent(displayParent);
+            Display.create();
+            initGL();
+        } catch (LWJGLException e) {
+        }
+        loader = new Loader();
+        masterRenderer = new MasterRenderer(loader);
+        camera = new Camera(null);
+        mousePicker = new MousePicker(camera, masterRenderer.getProjectionMatrix(), terrainModel);
+        gameLoop();
+    }
+
+    protected void initGL() {}
+
+    protected void gameLoop() {
+        while (running) {
+            Display.update();
+
+            if (masterRenderer != null) {
+                camera.move(10);
+                mousePicker.update();
+                masterRenderer.renderScene(terrainModel, new LinkedList<GraphicalEntity>(), new LinkedList<GraphicalEntity>(), lights,
+                    camera, 10, new Vector4f(0, 1, 0, 100000));
+            } else {
+                if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+                    GL11.glClearColor(1f, 0, 0, 0);
+                } else {
+                    GL11.glClearColor(1f, 1f, 0f, 0f);
+                }
+                GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+            }
+            String message = messageQueue.poll();
+            if (message != null) {
+                loadMap();
+            }
+            Display.sync(10);
+        }
+        Display.destroy();
+    }
+
+    /**
+     * Stop.
+     */
+    public void stop() {
+        running = false;
+    }
+
+    public void loadMap() {
+        loadTerrain("map");
+    }
+
+    private void loadTerrain(String mapName) {
+        Terrain terrain = TerrainLoader.loadTerrain(mapName + ".psc");
+        TerrainTexture backgroundTex = new TerrainTexture(loader.loadTexture(terrain.getBgTexture()));
+        TerrainTexture rTex = new TerrainTexture(loader.loadTexture(terrain.getRTexture()));
+        TerrainTexture gTex = new TerrainTexture(loader.loadTexture(terrain.getGTexture()));
+        TerrainTexture bTex = new TerrainTexture(loader.loadTexture(terrain.getBgTexture()));
+        TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTex, rTex, gTex, bTex);
+        TerrainTexture blendMap = new TerrainTexture(loader.loadTexture(TerrainLoader.createBlendMap(terrain)));
+
+        terrainModel = new TerrainModel(terrain, 0, 0, texturePack, blendMap, loader);
+        lights = terrain.getStaticLights();
+        // if (staticEntities == null) {
+        // staticEntities = new TreeMap<>();
+        // }
+        // staticEntities.putAll(terrain.getStaticObjects());
+    }
+
+}
