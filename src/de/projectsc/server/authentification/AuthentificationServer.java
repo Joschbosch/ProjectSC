b463fab11fff7cc2b12537f6774199ee6c4d240b
David Scholz <scholz.david93@gmail.com>
2015-08-16 18:50:56 +0200

    implemented simple authentification response. TODO: need to iterate through whole list which is very bad..

--- src/de/projectsc/server/authentification/AuthentificationServer.java
+++ src/de/projectsc/server/authentification/AuthentificationServer.java
@@ -4,10 +4,23 @@
  
 package de.projectsc.server.authentification;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.projectsc.server.authentification.api.AuthentificationReader;
+import de.projectsc.server.authentification.api.AuthentificationReader.Line;
+import de.projectsc.server.authentification.database.DatabaseConnection;
+import de.projectsc.server.authentification.impl.reader.CSVAuthentificationReader;
 import de.projectsc.server.core.messages.AuthentificationRequestServerMessage;
+import de.projectsc.server.core.messages.AuthentificationResponseServerMessage;
+import de.projectsc.server.core.messages.RequestEnum;
 import de.projectsc.server.core.messages.ServerMessage;
 
 /**
@@ -17,10 +30,40 @@ import de.projectsc.server.core.messages.ServerMessage;
  */
 public class AuthentificationServer {
     
+    private static final String PATH = "";
+    
+    private static final Log LOGGER = LogFactory.getLog(DatabaseConnection.class);
+    
     private final BlockingQueue<ServerMessage> authentificationQueue;
     
     public AuthentificationServer() {
         this.authentificationQueue = new LinkedBlockingQueue<>();
+    }
+    
+    public void handleAuthentificationRequests() {
+        ServerMessage msg = authentificationQueue.poll();
+        
+        if (msg instanceof AuthentificationRequestServerMessage) {
+            AuthentificationRequestServerMessage authetificationMsg = (AuthentificationRequestServerMessage) msg;
+            Map<RequestEnum, String> requestMap = authetificationMsg.getRequest();
+            try {
+                AuthentificationReader reader = new CSVAuthentificationReader(new FileReader(new File(PATH)), null);
+                Line l = reader.next();
+                ServerMessage response = null;
+                if (l.get("Name").equals(requestMap.get(RequestEnum.NAME)) && 
+                    l.get("Email").equals(requestMap.get(RequestEnum.EMAIL)) && 
+                    l.get("Password").equals(requestMap.get(RequestEnum.PASSWORD))) {
+                    response = new AuthentificationResponseServerMessage(authetificationMsg.getClient(), true);
+                } else {
+                    response = new AuthentificationResponseServerMessage(authetificationMsg.getClient(), false);
+                }
+                authentificationQueue.put(response);
+            } catch (IOException e) {
+                LOGGER.error(e);
+            } catch (InterruptedException e) {
+                LOGGER.error(e);
+            }
+        }
     }
     
     public BlockingQueue<ServerMessage> getAuthentificationQueue() {
