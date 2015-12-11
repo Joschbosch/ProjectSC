28ef8c9342bb3a0c97c6d44b2094f64ac7337680
Josch <Josch@Josch.koeln>
2015-12-11 12:11:20 +0100

    Clean up 4

---
+++ src/de/projectsc/modes/client/gui/GUIConstants.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015
+ */
+
+package de.projectsc.modes.client.gui;
+
+/**
+ * Constants for the GUI part.
+ * 
+ * @author Josch Bosch
+ */
+public final class GUIConstants {
+
+    /**
+     * Constant.
+     */
+    public static final String SKYBOX_IMAGE_ROOT = "";
+
+    /**
+     * Constant.
+     */
+    public static final String TEXTURE_ROOT = "/textures/";
+
+    /**
+     * Constant.
+     */
+    public static final String BASIC_TEXTURE_ROOT = "basic/";
+
+    /**
+     * Constant.
+     */
+    public static final String BASIC_TEXTURE_WHITE = TEXTURE_ROOT + BASIC_TEXTURE_ROOT + "white.png";
+
+    /**
+     * Constant.
+     */
+    public static final String BASIC_TEXTURE_BLACK = TEXTURE_ROOT + BASIC_TEXTURE_ROOT + "black.png";
+
+    /**
+     * Constant.
+     */
+    public static final String BASIC_MESH_ROOT = "/meshes/";
+
+    /**
+     * Constant.
+     */
+    public static final String BASIC_MESH_PRIMITIVES_ROOT = "primitives/";
+
+    /**
+     * Constant.
+     */
+    public static final String BASIC_MESH_PRIMITIVES_SPHERE = BASIC_MESH_ROOT + BASIC_MESH_PRIMITIVES_ROOT + "sphere.obj";
+
+    private GUIConstants() {
+
+    }
+}
